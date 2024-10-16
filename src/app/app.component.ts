93dd88f13f857179669968fdc962addfd1ba0907
andre <Andrew.d.cogburn@gmail.com>
2024-10-15 20:55:53 -0500

    beginning of background reveal effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  introduction = "Hello, I'm";
-  name = "Andrew Cogburn";
-  aboutMe = " and I am a web dev from Houston, TX.";
+  introduction: string = "Hello, I'm";
+  name: string = "Andrew Cogburn";
+  aboutMe: string = "a Web Developer from Houston, TX.";
 
   projects = [
     { id: 1, name: 'Cogburn Books', description: 'An e-commerce website hosting language education novellas, built by me using .NET and Angular.'},
     { id: 2, name: 'Biblingo', description: 'A language learning platform where I worked on the flashcard deck business rules.'}
   ];
+  
+  mouseX: number = 0;
+  mouseY: number = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+    onMouseMove(event: MouseEvent) {
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+    }
 }
